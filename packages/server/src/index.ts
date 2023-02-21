efa7c40b1f6bf6b55aded94cce9a6fa5fcfdee91
skar07 <sanskarpandey0@gmail.com>
2023-02-21 19:31:38 +0530

    feat: add DB connection

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 
+import { connectDB } from './db/connect';
+import { booksRouter } from './routes/books'
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
@@ -13,7 +16,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
+      res.send('<h1>Hello from the TypeScript world!</h1>');
 });
 
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+app.use('/api/v1/books', booksRouter);
+
+const start = async () => {
+      try{
+            await connectDB(process.env.MONGO_URI as string);
+            app.listen(PORT, () => console.log(`Server is listening port ${PORT}...`));
+      } catch (error) {
+            console.log(error)
+      }
+}
+start();
