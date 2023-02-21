904b614330f08461c36ccb09d1189345b90337bb
skar07 <sanskarpandey0@gmail.com>
2023-02-21 19:30:59 +0530

    style: implement result page and home pag styling

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -8,22 +8,25 @@ export default function Home() {
                         <h1 className="text-center text-3xl md:text-5xl font-bold underline">
                               Welcome! Find a book that matches your personality.
                         </h1>
-                        <section className='text-center p-5 md:p-10'>
-                              <h2>Please enter the following details:</h2>
-                              <form className="flex flex-col text-center mt-5 space-y-3">
-                                    <label>
+                        <section className='flex justify-center flex-col p-5 md:p-10 text-center'>
+                              <h2 className="text-2xl">Please enter the following details:</h2>
+                              <form className="flex flex-col text-center justify-center mt-5 space-y-3 font-medium text-xl"
+                                    action="/result"
+                                    method="post"
+                              >
+                                    <label className="px-2">
                                           Age:
-                                          <input type='number' name='age' />
+                                          <input type='number' name='age' className="mx-3 p-2 border-2 border-dark-purple rounded-md focus:outline-none"/>
                                     </label>
                                     <label>
                                           Gender:
-                                          <select name='gender'>
+                                          <select name='gender' className="p-2 mx-3 rounded-md border-2 border-claret hover:border-dark-purple hover:text-dark-purple">
                                                 <option value='male'>Male</option>
                                                 <option value='female'>Female</option>
                                                 <option value='other'>Other</option>
                                           </select>
                                     </label>
-                                    <input type='submit' name='submit' />
+                                    <input type='submit' name='submit' className="p-3 text-center bg-robin-egg-blue w-1/6"/>
                               </form>
                         </section>
                   </div>
--- packages/client/pages/result.tsx
+++ packages/client/pages/result.tsx
@@ -4,8 +4,9 @@ import React from 'react';
 export default function Result() {
       return(
             <Page title='Result' navigation='result'>
-                  <div>
-                        
+                  <div className='p-20 h-screen bg-dark-purple text-center'>
+                        <div className='md:p-20 w-full md:w-auto h-4/5 bg-baby-powder'>
+                        </div>
                   </div>
             </Page>
       )
