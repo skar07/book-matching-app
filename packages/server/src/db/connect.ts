7d974fb80f1c0c7b6a7903e670e2405219374699
skar07 <sanskarpandey0@gmail.com>
2023-02-22 20:52:36 +0530

    refactor: add MONGO_URI to connectDB

--- packages/server/src/db/connect.ts
+++ packages/server/src/db/connect.ts
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const connectDB = (url: string) => {
-      return mongoose.connect(url);
+export const connectDB = () => {
+      return mongoose.connect(process.env.MONGO_URI as string);
 }
